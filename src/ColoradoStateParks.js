ded6fb35e4f0f1f5b7da7ec3a4eafa3a722fc534
Hyun Sung Cho <hcho774@gmail.com>
2022-04-25 09:37:49 -0400

    commit tree 15e3552fbc307f43b28f3bcbe9e1f1d13acb39dc

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+
+console.log(trees);
+wildlife();
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+ // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>)
 }
+
+export default ColoradoStateParks;
